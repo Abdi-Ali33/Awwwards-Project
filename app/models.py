cee66ba49a8499648edbe03784e33c31725a27a7
Abdi Ali <alikune33@gmail.com>
2022-06-13 18:19:08 +0300

    updating models'

--- app/models.py
+++ app/models.py
@@ -7,8 +7,9 @@ import datetime as dt
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='images/')
     bio = models.TextField(max_length=500, default="My Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
@@ -33,7 +34,8 @@ class Post(models.Model):
     description = models.TextField(max_length=255)
     technologies = models.CharField(max_length=200, blank=True)
     photo = ImageField(manual_crop='1280x720')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts")
     date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
@@ -52,8 +54,6 @@ class Post(models.Model):
 
     def save_post(self):
         self.save()
-
-
 
 
 class Rating(models.Model):
@@ -77,8 +77,10 @@ class Rating(models.Model):
     design_average = models.FloatField(default=0, blank=True)
     usability_average = models.FloatField(default=0, blank=True)
     content_average = models.FloatField(default=0, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='ratings', null=True)
 
     def save_rating(self):
         self.save()
